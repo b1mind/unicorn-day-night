0aedcffdddb35d4145a52c54b34afcd0b6148fa0
b1mind <brent@1mindsolutions.com>
2021-03-12 14:18:54 -0700

    🐣 in the begining

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // build options? using es build
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
+  },
+
+  mount: {
+    public: '/',
+  },
+
+  plugins: [
+    // '@snowpack/plugin-svelte',
+    ['@snowpack/plugin-sass', {}],
+    ['@marlonmarcello/snowpack-plugin-pug', {}],
+    '@snowpack/plugin-postcss',
+  ],
+
+  devOptions: {
+    port: 9001,
+    open: 'none',
+  },
+
+  buildOptions: {
+    baseUrl: './',
+  },
+}
