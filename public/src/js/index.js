1409dd1e54da3683172e0568c56a2d34801df10f
b1mind <brent@1mindsolutions.com>
2021-03-13 03:19:05 -0700

    📜 scolltrigger added

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,13 +1,16 @@
 console.log('working?')
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const setTimeBtn = document.getElementById('setTimeBtn')
 const inputTime = document.getElementById('inputTime')
 const clock = document.getElementById('clock')
 const day = document.getElementById('day')
 const night = document.getElementById('night')
-
 const scene = document.querySelector('#scene')
+const body = document.querySelector('body')
 
 let sunRise = '06:30'
 let sunSet = '19:30'
@@ -29,21 +32,35 @@ const rotateClock = angle => {
   })
 }
 
+const scrollClock = gsap.to(clock, {
+  rotation: '360deg',
+  transformOrigin: 'center center',
+  scrollTrigger: {
+    trigger: 'main',
+    pin: true,
+    scrub: 1,
+    markers: true,
+    end: '+=4200 top',
+  },
+})
+
+console.dir(rotateClock)
+
 const dayTime = time => {
-  bg.classList.replace('night-time', 'day')
+  body.classList.replace('night-time', 'day')
   console.log(`do other day stuff its ${time}`)
-  gsap.to(bg, { duration: 1.5, fill: 'rgb(63, 106, 245)' })
+  gsap.to(body, { duration: 1.5, backgroundColor: '#80C2FF' })
   gsap.fromTo(
     day,
     { duration: 1, rotation: 0 },
-    { rotation: 360, transformOrigin: 'center center' },
+    { rotation: 360, transformOrigin: 'center' },
   )
 }
 
 const nightTime = time => {
-  bg.classList.replace('day', 'night-time')
+  body.classList.replace('day', 'night-time')
   console.log(`do other night stuffs its ${time}`)
-  gsap.to(bg, { duration: 0.5, fill: '#031758' })
+  gsap.to(body, { duration: 1.5, backgroundColor: '#031758' })
   gsap.fromTo(
     night,
     { duration: 1, rotation: 0 },
@@ -51,23 +68,23 @@ const nightTime = time => {
   )
 }
 
-const setTime = (e = timeNow) => {
-  let time = e
+const setTime = (time = timeNow) => {
+  inputTime.value = time
   time <= sunSet && time >= sunRise ? dayTime(time) : nightTime(time)
-  let angle = findAngle(time)
+  const angle = findAngle(time)
   rotateClock(angle)
 }
 
 const updateTime = () => {
-  let userSetTime = inputTime.value
+  const userSetTime = inputTime.value
   userSetTime ? setTime(userSetTime) : console.log(`User Set Incomplete time`)
 }
 
 function resize(e) {
-  let mobile = window.matchMedia('(max-width: 360px)')
+  const mobile = window.matchMedia('(max-width: 420px)')
   mobile.matches
-    ? gsap.set(scene, { attr: { viewBox: '300 0 1020 600' } })
-    : gsap.set(scene, { attr: { viewBox: '0 0 1020 600' } })
+    ? gsap.set(scene, { attr: { viewBox: '250 0 1220 603' } })
+    : gsap.set(scene, { attr: { viewBox: '100 0 800 603' } })
 }
 
 setTimeBtn.addEventListener('click', updateTime)
@@ -77,7 +94,5 @@ window.addEventListener('resize', e => {
 })
 
 // invoke
-inputTime.value = timeNow
 setTime()
-
 resize()
